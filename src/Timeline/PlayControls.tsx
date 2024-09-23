f59a6b4f2c99452e7d5adc8681a23a8944eda34e
sleepingkirby <sleepingkirby@gmail.com>
2024-09-23 16:04:38 +0800

    added onkeypress enter and onblur

--- src/Timeline/PlayControls.tsx
+++ src/Timeline/PlayControls.tsx
@@ -8,12 +8,40 @@ type PlayControlsProps = {
 export const PlayControls = ({ time, setTime }: PlayControlsProps) => {
   // TODO: implement time <= maxTime
 
+  const validateTime = (n: number, max: number) => {
+    let num: number = Number(n);
+    let mx: number = Number(max);
+    num = num > mx ? mx : num;
+    num = num < 0 ? 0 : num;
+    return num;
+  } 
+
   const onTimeChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       setTime(Number(e.target.value));
     },
     [setTime],
   );
+
+  const onValidateTime = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if(e.key === "Enter"){
+      const t = e.target as HTMLInputElement;
+      setTime(validateTime(time, Number(t?.max)));
+      }
+    },
+    [time, setTime, validateTime],
+  );
+
+  const onBlurTime = useCallback(
+    (e: React.FocusEvent<HTMLInputElement>) => {
+      console.log(e);
+      const t = e.target as HTMLInputElement;
+      setTime(validateTime(time, Number(t?.max)));
+    },
+    [time, setTime, validateTime],
+  );
+
 
   return (
     <div
@@ -32,6 +60,8 @@ export const PlayControls = ({ time, setTime }: PlayControlsProps) => {
           step={10}
           value={time}
           onChange={onTimeChange}
+          onKeyPress={onValidateTime}
+          onBlur={onBlurTime}
         />
       </fieldset>
       -
