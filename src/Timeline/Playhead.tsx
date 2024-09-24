80c8e8d257c6aee0e7ea7c8f67b3a7f67e99fb96
sleepingkirby <sleepingkirby@gmail.com>
2024-09-24 08:16:44 +0800

    playhead hidden when scroll is less than zero

--- src/Timeline/Playhead.tsx
+++ src/Timeline/Playhead.tsx
@@ -1,13 +1,15 @@
 type PlayheadProps = {
   time: number;
+  scrollTimeRuler: number;
 };
 
-export const Playhead = ({ time }: PlayheadProps) => {
+export const Playhead = ({ time, scrollTimeRuler }: PlayheadProps) => {
+  
   return (
     <div
       className="absolute left-[316px] h-full border-l-2 border-solid border-yellow-600 z-10"
       data-testid="playhead"
-      style={{ transform: `translateX(calc(${time}px - 50%))` }}
+      style={{ transform: `translateX(calc(${time - scrollTimeRuler}px - 50%))`, visibility:`${time - scrollTimeRuler < 0 ? 'hidden' : 'visible'}` }}
     >
       <div className="absolute border-solid border-[5px] border-transparent border-t-yellow-600 -translate-x-1.5" />
     </div>
